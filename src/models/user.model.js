8c6a867beb99f0cb05b7899382fa71984a6972cd
navin-jha <njha55360@gmail.com>
2026-01-09 22:24:16 +0530

    configure bcryptjs and jsonwebtoken for hashing and authentication

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,6 @@
 import { Schema, model } from "mongoose";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 const userShcema = new Schema(
   {
@@ -18,5 +20,34 @@ const userShcema = new Schema(
   },
   { timestamps: true }
 );
+
+userShcema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+userShcema.methods.comparePassword = async function (candidatePassword) {
+  return await bcrypt.compare(candidatePassword, this.password);
+};
+
+userShcema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    { id: this._id, fullname: this.fullname, email: this.email },
+    process.env.ACCESS_TOKEN_SECRET,
+    { expiresIn: process.env.ACCESS_TOKEN_EXPIRY }
+  );
+};
+
+userShcema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    { id: this._id },
+    process.env.REFRESH_TOKEN_SECRET,
+    { expiresIn: process.env.REFRESH_TOKEN_EXPIRY }
+  );
+};
 
 export const User = model("User", userShcema);
